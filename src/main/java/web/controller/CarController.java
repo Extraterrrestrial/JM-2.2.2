63071c29b0e5f1eb134d071b90be8105ef9458e3
Yuri Kriger <yuri.kriger@gmail.com>
2021-10-30 19:18:42 +0600

    some changes

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -16,8 +16,18 @@ public class CarController {
     }
 
     @GetMapping(value = "/cars")
-    public String printAllCars(@RequestParam(value = "count", required = false) int count, Model model) {
+//    public String printAllCars(@RequestParam(value = "count", required = false) int count, Model model) {
+    public String printAllCars(@RequestParam(defaultValue = "5") int count, Model model) {
         model.addAttribute("cars", carService.listCars(count));
         return "cars";
     }
+
+//    @GetMapping(value = "/cars")
+//    public String printCars(@RequestParam(defaultValue="5") Integer count, ModelMap model) {
+//        CarServiceImpl carsServiceImpl = new CarServiceImpl();
+//        List<Car> qtyCars = carsServiceImpl.carList(count);
+//        model.addAttribute("cars", qtyCars);
+//        return "cars";
+//}
+
 }
